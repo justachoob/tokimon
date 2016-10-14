cc37f8e45af713bb4339004d066f6ac451adbd94
justachoob <justachoob@gmail.com>
2016-10-14 01:39:04 +0000

    Link Ups between Trainer, Tokimon complete... Proper calculation of total, level complete, to do: Display pages and roots

--- app/models/tokimon.rb
+++ app/models/tokimon.rb
@@ -1,3 +1,20 @@
 class Tokimon < ApplicationRecord
   belongs_to :trainer
+  
+  validates :tkname, presence: true
+  validates :weight, presence: true
+  validates :height, presence: true
+  validates :fly, presence: true
+  validates :fight, presence: true
+  validates :fire, presence: true
+  validates :water, presence: true
+  validates :electric, presence: true
+  validates :ice, presence: true
+  
+  def total
+    self.fly + self.fight + self.fire + self.water + self.electric + self.ice
+  end
+
+
+  
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,4 +1,14 @@
 class Trainer < ApplicationRecord
     has_many :tokimons
     
+    validates :trname, presence: true
+    validates :gender, presence: true
+    validates :country, presence: true
+
+    def level
+        (self.tokimons.length/3).round
+    end
+    
+  
+    
 end
